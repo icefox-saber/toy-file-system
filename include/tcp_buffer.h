2a4d72be3835ccb5d94b44d8117e08e959457f2d
icefox <728862050a@gmail.com>
2024-04-23 16:33:19 +0800

    添加了注释

--- include/tcp_buffer.h
+++ include/tcp_buffer.h
@@ -7,15 +7,81 @@ typedef struct tcp_buffer {
     int read_index;
     int write_index;
     char buf[TCP_BUF_SIZE];
-    // char *buf_ptr;
 } tcp_buffer;
 
+/**
+ * @brief  Initialize a buffer
+ *
+ * Initializes a buffer.
+ *
+ * @return tcp_buffer    created buffer
+ */
 tcp_buffer *init_buffer();
-void adjust_buffer(tcp_buffer *buf);
-void recycle_write(tcp_buffer *buf, int len);
-void recycle_read(tcp_buffer *buf, int len);
+
+/**
+ * @brief  Send to buffer
+ *
+ * Send a string to the buffer.
+ * The string will be regarded as a whole data packet.
+ * The first 4 bytes of the message will be the length of the message.
+ *
+ * @param  buf   buffer to be written
+ * @param  s     string to be written
+ * @param  len   length of the string
+ */
 void send_to_buffer(tcp_buffer *buf, const char *s, int len);
+
+/**
+ * @brief  Buffer input
+ *
+ * Read all the data from the socket and write to the buffer.
+ *
+ * @param  buf     buffer to be written
+ * @param  sockfd  socket to be read
+ *
+ * @return int     the number of bytes read, -1 if error or closed
+ */
 int buffer_input(tcp_buffer *buf, int sockfd);
+
+/**
+ * @brief  Buffer output
+ *
+ * Write all the data in the buffer to the socket.
+ *
+ * @param  buf     buffer to be read
+ * @param  sockfd  socket to be written
+ */
 void buffer_output(tcp_buffer *buf, int sockfd);
+
+/**
+ * @brief  Adjust buffer
+ *
+ * If read_index is larger than TCP_BUF_SIZE / 2,
+ * move the data to the beginning of the buffer.
+ * Used after recycle_read and recycle_write.
+ *
+ * @param  buf   buffer to be adjusted
+ */
+void adjust_buffer(tcp_buffer *buf);
+
+/**
+ * @brief  Recycle write
+ *
+ * Move the write_index by len.
+ *
+ * @param  buf   buffer to be adjusted
+ * @param  len   length to be moved
+ */
+void recycle_write(tcp_buffer *buf, int len);
+
+/**
+ * @brief  Recycle read
+ *
+ * Move the read_index by len.
+ *
+ * @param  buf   buffer to be adjusted
+ * @param  len   length to be moved
+ */
+void recycle_read(tcp_buffer *buf, int len);
 
 #endif
--- step1/BDS.c
+++ step1/BDS.c
@@ -12,8 +12,7 @@
 int ncyl, nsec, ttd;
 
 // return a negative value to exit
-int cmd_i(tcp_buffer *write_buf, char *args, int len)
-{
+int cmd_i(tcp_buffer *write_buf, char *args, int len) {
     static char buf[64];
     sprintf(buf, "%d %d", ncyl, nsec);
 
@@ -22,26 +21,22 @@ int cmd_i(tcp_buffer *write_buf, char *args, int len)
     return 0;
 }
 
-int cmd_r(tcp_buffer *write_buf, char *args, int len)
-{
+int cmd_r(tcp_buffer *write_buf, char *args, int len) {
     send_to_buffer(write_buf, args, len);
     return 0;
 }
 
-int cmd_w(tcp_buffer *write_buf, char *args, int len)
-{
+int cmd_w(tcp_buffer *write_buf, char *args, int len) {
     send_to_buffer(write_buf, "Yes", 4);
     return 0;
 }
 
-int cmd_e(tcp_buffer *write_buf, char *args, int len)
-{
+int cmd_e(tcp_buffer *write_buf, char *args, int len) {
     send_to_buffer(write_buf, "Bye!", 5);
     return -1;
 }
 
-static struct
-{
+static struct {
     const char *name;
     int (*handler)(tcp_buffer *write_buf, char *, int);
 } cmd_table[] = {
@@ -53,43 +48,35 @@ static struct
 
 #define NCMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
-void add_client(int id)
-{
+void add_client(int id) {
     // some code that are executed when a new client is connected
     // you don't need this in step1
 }
 
-int handle_client(int id, tcp_buffer *write_buf, char *msg, int len)
-{
+int handle_client(int id, tcp_buffer *write_buf, char *msg, int len) {
     char *p = strtok(msg, " \r\n");
     int ret = 1;
     for (int i = 0; i < NCMD; i++)
-        if (strcmp(p, cmd_table[i].name) == 0)
-        {
+        if (strcmp(p, cmd_table[i].name) == 0) {
             ret = cmd_table[i].handler(write_buf, p + strlen(p) + 1, len - strlen(p) - 1);
             break;
         }
-    if (ret == 1)
-    {
+    if (ret == 1) {
         static char unk[] = "Unknown command";
         send_to_buffer(write_buf, unk, sizeof(unk));
     }
-    if (ret < 0)
-    {
+    if (ret < 0) {
         return -1;
     }
 }
 
-void clear_client(int id)
-{
+void clear_client(int id) {
     // some code that are executed when a client is disconnected
     // you don't need this in step2
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc < 5)
-    {
+int main(int argc, char *argv[]) {
+    if (argc < 5) {
         fprintf(stderr,
                 "Usage: %s <disk file name> <cylinders> <sector per cylinder> "
                 "<track-to-track delay> <port>\n",
@@ -100,7 +87,7 @@ int main(int argc, char *argv[])
     char *diskfname = argv[1];
     ncyl = atoi(argv[2]);
     nsec = atoi(argv[3]);
-    ttd = atoi(argv[4]); // ms
+    ttd = atoi(argv[4]);  // ms
     int port = atoi(argv[5]);
 
     // open file
