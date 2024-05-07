f52fe50f4d529e4ba161fdfb1b303127b61513b7
icefox <728862050a@gmail.com>
2024-05-07 21:19:05 +0800

    FD TO FP

--- step1/BDS.c
+++ step1/BDS.c
@@ -10,9 +10,10 @@
 //  Block size in bytes
 #define BLOCKSIZE 256
 int ncyl, nsec, ttd;
-int currentcyl=0;
-char *diskfname;//diskfname
-int fd;//全局文件描述符
+int currentcyl = 0;
+char *diskfname; //diskfname
+FILE *fp;         //全局文件指针
+
 // return a negative value to exit
 // information
 // args和len没用，为了模板化加的参数
@@ -26,57 +27,61 @@ int cmd_i(tcp_buffer *write_buf, char *args, int len)
     send_to_buffer(write_buf, buf, strlen(buf) + 1);
     return 0;
 }
+
 // read cylinder sector args len
 int cmd_r(tcp_buffer *write_buf, char *args, int len)
 {
-     static char *msg0="error command";
+    static char *msg0 = "error command";
     static char buf[BLOCKSIZE];
     char *c = strtok(args, " ");
     char *s = strtok(NULL, " ");
-    if(!c||!s)
+    if (!c || !s)
     {
         send_to_buffer(write_buf, msg0, strlen(msg0));
         return 0; // 或者返回错误代码
     }
-    int cylinder = atoi(c);//这里可能会异常即c为空
+    int cylinder = atoi(c); //这里可能会异常即c为空
     int sector = atoi(s);
-    usleep(ttd*abs(currentcyl-cylinder));
-    currentcyl=cylinder;
+    usleep(ttd * abs(currentcyl - cylinder));
+    currentcyl = cylinder;
     // 计算文件偏移量
-    off_t offset = (cylinder * nsec + sector) * BLOCKSIZE;
+    long offset = (cylinder * nsec + sector) * BLOCKSIZE;
 
-    static char *msg1 = "Error calling lseek() to set file offset";
-    static char *msg2 = "Error writing to file in cmd_w";
+    static char *msg1 = "Error calling fseek() to set file offset";
+    static char *msg2 = "Error reading from file in cmd_r";
     // 定位文件偏移量
-    if (lseek(fd, offset, SEEK_SET) == -1)
+    if (fseek(fp, offset, SEEK_SET) == -1)
     {
         send_to_buffer(write_buf, msg1, strlen(msg1));
         return 0; // 或者返回错误代码
     }
 
     // 读取数据
-    if (read(fd, buf, BLOCKSIZE) == -1)
+    size_t num = fread(buf, 1, BLOCKSIZE, fp); //这里即使是空也会读入256个字符
+    if (num == 0)
     {
-        send_to_buffer(write_buf, msg2, strlen(msg2));
+        send_to_buffer(write_buf, "nothing", strlen(msg2));
         return 0; // 或者返回错误代码
     }
-
+	char *megsyes="yes";
+	char *res=strcat(buf,megsyes);
     // 发送数据到缓冲区
-    send_to_buffer(write_buf, buf, strlen(buf));
+    send_to_buffer(write_buf, res, num);
     return 0;
 }
-//W 
+
+//W
 //args:cylinder sector len data
 //len:strlen(args)
 int cmd_w(tcp_buffer *write_buf, char *args, int len)
 {
-    static char *msg0="error command";
+    static char *msg0 = "error command";
     //static char buf[BLOCKSIZE];
     char *c = strtok(args, " ");
     char *s = strtok(NULL, " ");
-    
+
     char *l = strtok(NULL, " ");
-    if(!c||!s||!l)
+    if (!c || !s || !l)
     {
         send_to_buffer(write_buf, msg0, strlen(msg0));
         return 0; // 或者返回错误代码
@@ -86,24 +91,25 @@ int cmd_w(tcp_buffer *write_buf, char *args, int len)
     int datalen = atoi(l);
     char *d = strtok(NULL, "\0");
     // 计算文件偏移量
-    usleep(ttd*abs(currentcyl-cylinder));
-    currentcyl=cylinder;
-    off_t offset = (cylinder * nsec + sector) * BLOCKSIZE;
-    static char *msg1 = "Error calling lseek() to set file offset";
+    usleep(ttd * abs(currentcyl - cylinder));
+    currentcyl = cylinder;
+    long offset = (cylinder * nsec + sector) * BLOCKSIZE;
+
+    static char *msg1 = "Error calling fseek() to set file offset";
     static char *msg2 = "Error writing to file in cmd_w";
     // 定位文件偏移量
-    off_t lastoffset = 0;
-    
-    if (lseek(fd, offset, SEEK_SET) == -1)
+
+    if (fseek(fp, offset, SEEK_SET) == -1)
     {
         send_to_buffer(write_buf, msg1, strlen(msg1));
         return 0; // 或者返回错误代码
     }
 
     // 写入数据
-    if (write(fd, d, datalen) == -1)
+    size_t written = fwrite(d, 1, datalen, fp);
+    if (written != datalen)
     {
-        send_to_buffer(write_buf,msg2,strlen(msg2));
+        send_to_buffer(write_buf, msg2, strlen(msg2));
         return 0; // 或者返回错误代码
     }
 
@@ -141,7 +147,7 @@ void add_client(int id)
 
 int handle_client(int id, tcp_buffer *write_buf, char *msg, int len)
 {
-    
+
     char *p = strtok(msg, " \r\n");
     int ret = 1;
     for (int i = 0; i < NCMD; i++)
@@ -191,8 +197,8 @@ int main(int argc, char *argv[])
 
     // open file
     char *filename = diskfname;
-    fd = open(filename, O_RDWR | O_CREAT, 0);
-    if (fd < 0)
+    fp = fopen(filename, "w+");
+    if (!fp)
     {
         printf("Error: Could not open file '%s'.\n", filename);
         exit(-1);
@@ -200,18 +206,18 @@ int main(int argc, char *argv[])
 
     // stretch the file
     long FILESIZE = BLOCKSIZE * nsec * ncyl;
-    int result = lseek(fd, FILESIZE - 1, SEEK_SET);
+    int result = fseek(fp, FILESIZE - 1, SEEK_SET);
     if (result == -1)
     {
-        perror("Error calling lseek() to 'stretch' the file");
-        close(fd);
+        perror("Error calling fseek() to 'stretch' the file");
+        fclose(fp);
         exit(-1);
     }
-    result = write(fd, "", 1);
+    result = fwrite("", 1, 1, fp);
     if (result != 1)
     {
         perror("Error writing last byte of the file");
-        close(fd);
+        fclose(fp);
         exit(-1);
     }
 
