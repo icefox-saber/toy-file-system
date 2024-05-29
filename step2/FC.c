3d7a82a7b9cca5427717d1f1009d127c99d55e32
icefox <728862050a@gmail.com>
2024-05-29 19:44:54 +0800

    完成了多用户同时

--- step2/FC.c
+++ step2/FC.c
@@ -18,10 +18,23 @@ int main(int argc, char *argv[])
     int port = atoi(argv[1]);
     tcp_client client = client_init("localhost", port);
     static char buf[4096];
-    char *ini="B";
+    char ini[20] = "ini\n";
+    printf("uid:");
+    fgets(buf, sizeof(buf), stdin);
+    strncat(ini, buf, 3);
     client_send(client, ini, strlen(ini) + 1);
     int n = client_recv(client, buf, sizeof(buf));
     buf[n] = 0;
+    if (strcmp(buf, "Please first format.\n!format$") == 0)
+    {
+        client_send(client, "f\n", 2);
+        n = client_recv(client, buf, sizeof(buf));
+        buf[n] = 0;
+        client_send(client, ini, strlen(ini) + 1);
+        n = client_recv(client, buf, sizeof(buf));
+        buf[n] = 0;
+    }
+
     printf("%s", buf);
     // int n = client_recv(client, buf, sizeof(buf));
     // buf[n] = 0;
@@ -32,7 +45,7 @@ int main(int argc, char *argv[])
         if (feof(stdin))
             break;
         client_send(client, buf, strlen(buf) + 1);
-        int n = client_recv(client, buf, sizeof(buf));
+        n = client_recv(client, buf, sizeof(buf));
         buf[n] = 0;
         printf("%s", buf);
         if (strcmp(buf, "Goodbye!") == 0)
--- step2/FS.c
+++ step2/FS.c
@@ -9,7 +9,6 @@
 #include <stdint.h>
 #include <regex.h>
 #include "tcp_utils.h"
-
 #include "superblock.h"
 #include "inode.h"
 #include "diskop.h"
@@ -23,9 +22,10 @@ int server_socket;
 int disk_server_socket;
 char response[RESPONSE_LENGTH] = "";
 
-uint16_t current_dir = 65535;                    // 当前目录结点编号
-char cur_path_string[MAX_PATH_STR_LENGTH] = "$"; // 当前目录字符串
-bool formatted = false;                          // 是否格式化
+uint16_t current_dir[256] = {65535};                   // 当前目录结点编号
+char cur_path_string[256][MAX_PATH_STR_LENGTH]; // 当前目录字符串
+uint8_t uid[256];
+bool formatted = false; // 是否格式化
 
 /*增加到response*/
 void _printf(const char *format, ...)
@@ -64,7 +64,7 @@ int check_name(const char *text)
         // 检查名称长度
         if (strlen(text) > MAX_NAME_SIZE)
         {
-            printf("Illegal name! (Must be shorter than 28)\n");
+            _printf("Illegal name! (Must be shorter than 28)\n");
             regfree(&regex);
             return 1;
         }
@@ -75,7 +75,7 @@ int check_name(const char *text)
     }
     else if (reti == REG_NOMATCH)
     {
-        printf("Illegal name! (Only accept numbers, letters, _ and .)\n");
+        _printf("Illegal name! (Only accept numbers, letters, _ and .)\n");
     }
     else
     {
@@ -90,8 +90,13 @@ int check_name(const char *text)
 }
 
 /** @brief 初始化文件系统 */
-int format_handle(char *params)
+int format_handle(char *params, uint8_t owner)
 {
+    if (owner != 0)
+    {
+        _printf("only user uid:0 can format");
+        return 0;
+    }
     if (init_spb() < 0)
     { // 初始化super block
         _printf("Init superblock failed. Initializing this file system requires at least 132 blocks of connected disks.\n");
@@ -102,11 +107,11 @@ int format_handle(char *params)
         _printf("Init root inode failed.\n");
         return 0;
     };
-    current_dir = 0; // 初始化当前目录为root
-    strcpy(cur_path_string, "/");
+    current_dir[owner] = 0; // 初始化当前目录为root
+    strcpy(cur_path_string[owner], "/");
     for (int i = 1; i < MAX_INODE_COUNT; i++)
     { // 初始化inode table
-        if (init_inode(&inode_table[i], i, 0, 0, 0, 65535) < 0||write_inode(&inode_table[i], i)<0)
+        if (init_inode(&inode_table[i], i, 0, 0, 0, 65535, 0, 0) < 0 || write_inode(&inode_table[i], i) < 0)
         {
             _printf("Init inode failed.\n");
             return 0;
@@ -127,71 +132,110 @@ int check_formatted()
 }
 /** @brief 创建文件
  * @param params 格式<f>，f为文件名*/
-int mk_handle(char *params)
+int mk_handle(char *params, uint8_t owner)
 {
-    if(!check_formatted())
+    if (!check_formatted())
         return 0;
+    char name[MAX_NAME_SIZE];
+    char *p = strtok(params, " ");
+    strcpy(name, p);
+    p = strtok(NULL, " ");
+    uint8_t lock;
+    if (p)
+    {
+        lock = atoi(p);
+    }
+    else
+    {
+        lock = 0;
+    }
     if (check_name(params))
     {
         return 0;
     } // 文件名非法
-    if (search_in_dir_inode(&inode_table[current_dir], params, 0) >= 0)
+    if (search_in_dir_inode(&inode_table[current_dir[owner]], params, 0, 0) >= 0)
     { // 重名
         _printf("A file with the same name already exists!\n");
         return 0;
     }
-    add_to_dir_inode(&inode_table[current_dir], params, 0);
+
+    if (!(lock <= 2) || !(lock >= 0))
+    {
+        _printf("lock level error");
+        return 0;
+    }
+    add_to_dir_inode(&inode_table[current_dir[owner]], name, 0, owner, lock);
     return 0;
 }
 
 /** @brief 创建文件夹
  * @param params 格式<f>，f为文件夹名*/
-int mkdir_handle(char *params)
+int mkdir_handle(char *params, uint8_t owner)
 {
-    if(!check_formatted())
+    if (!check_formatted())
         return 0;
+    char name[MAX_NAME_SIZE];
+    char *p = strtok(params, " ");
+    strcpy(name, p);
+    p = strtok(NULL, " ");
+    uint8_t lock;
+    if (p)
+    {
+        lock = atoi(p);
+    }
+    else
+    {
+        lock = 0;
+    }
+    if (lock > 2 || lock < 0)
+    {
+        _printf("lock level error");
+        return 0;
+    }
     if (check_name(params))
     {
         return 0;
     } // 文件夹名非法
-    if (search_in_dir_inode(&inode_table[current_dir], params, 1) >= 0)
+    if (search_in_dir_inode(&inode_table[current_dir[owner]], params, 1, 0) >= 0)
     { // 重名
         _printf("A sub-dir with the same name already exists!\n");
         return 0;
-    }add_to_dir_inode(&inode_table[current_dir], params, 1);
+    }
+
+    add_to_dir_inode(&inode_table[current_dir[owner]], name, 1, owner, lock);
     return 0;
 }
 
 /** @brief 删除文件
  * @param params 格式<f>，f为文件名*/
-int rm_handle(char *params)
+int rm_handle(char *params, uint8_t user)
 {
-    if(!check_formatted())
+    if (!check_formatted())
         return 0;
     if (check_name(params))
     {
         return 0;
     } // 文件名非法
-    rm_from_dir_inode(&inode_table[current_dir], params);
+    rm_from_dir_inode(&inode_table[current_dir[user]], params, user);
     return 0;
 }
 
 /** @brief 更改路径
  * @param params 格式<f>，f为路径字符串*/
-int cd_handle(char *params)
+int cd_handle(char *params, uint8_t user)
 {
     if (!check_formatted())
         return 0;
 
-    uint16_t ori_dir = current_dir; // 保存当前目录ID和路径字符串
+    uint16_t ori_dir = current_dir[user]; // 保存当前目录ID和路径字符串
     char ori_path_string[MAX_PATH_STR_LENGTH];
-    strcpy(ori_path_string, cur_path_string);
+    strcpy(ori_path_string, cur_path_string[user]);
 
     if (params[0] == '/')
     {
         // 处理绝对路径
-        current_dir = 0; // 根目录ID，假设根目录ID为0
-        strcpy(cur_path_string, "/");
+        current_dir[user] = 0; // 根目录ID，假设根目录ID为0
+        strcpy(cur_path_string[user], "/");
         params++; // 跳过第一个 '/'
     }
 
@@ -200,53 +244,52 @@ int cd_handle(char *params)
     {
         if (strcmp(path, "..") == 0)
         { // 回到上一级
-            uint16_t parent = inode_table[current_dir].i_parent;
+            uint16_t parent = inode_table[current_dir[user]].i_parent;
             if (parent != 65535)
             { // 65535表示无父目录或根目录
-                current_dir = parent;
+                current_dir[user] = parent;
 
-                if (strcmp(cur_path_string, "/") != 0)
+                if (strcmp(cur_path_string[user], "/") != 0)
                 {
-                    char *last_slash = strrchr(cur_path_string, '/');
+                    char *last_slash = strrchr(cur_path_string[user], '/');
                     if (last_slash != NULL)
                         *last_slash = '\0';
                 }
                 if (parent == 0)
-                    strcpy(cur_path_string, "/");
+                    strcpy(cur_path_string[user], "/");
             }
         }
         else if (strcmp(path, ".") != 0)
         { // . 代表当前目录，无视之
-            int result = search_in_dir_inode(&inode_table[current_dir], path, 1);
+            int result = search_in_dir_inode(&inode_table[current_dir[user]], path, 1, user);
             if (result == -1)
             {
                 _printf("Directory not found.\n");
-                current_dir = ori_dir; // 找不到对应文件夹，回退
-                strcpy(cur_path_string, ori_path_string);
+                current_dir[user] = ori_dir; // 找不到对应文件夹，回退
+                strcpy(cur_path_string[user], ori_path_string);
                 return 0;
             }
-            if (current_dir != 0 && strcmp(cur_path_string, "/") != 0)
-                strcat(cur_path_string, "/");
-            current_dir = result;
-            strcat(cur_path_string, path);
+            if (current_dir[user] != 0 && strcmp(cur_path_string[user], "/") != 0)
+                strcat(cur_path_string[user], "/");
+            current_dir[user] = result;
+            strcat(cur_path_string[user], path);
         }
         path = strtok(NULL, "/");
     }
     return 0;
 }
 
-
 /** @brief 删除文件夹
  * @param params 格式<f>，f为文件夹名*/
-int rmdir_handle(char *params)
+int rmdir_handle(char *params, uint8_t user)
 {
-    if(!check_formatted())
+    if (!check_formatted())
         return 0;
     if (check_name(params))
     {
         return 0;
     } // 文件名非法
-    if (rmdir_from_dir_inode(&inode_table[current_dir], params) < 0)
+    if (rmdir_from_dir_inode(&inode_table[current_dir[user]], params, user) < 0)
     {
         _printf("Failed: the possible reason is the dir is not empty!\n");
     }
@@ -254,9 +297,9 @@ int rmdir_handle(char *params)
 }
 
 /** @brief 列出文件夹和文件名称*/
-int ls_handle(char *params)
+int ls_handle(char *params, uint8_t user)
 {
-    if(!check_formatted())
+    if (!check_formatted())
         return 0;
     char buf[64 * MAX_NAME_SIZE];
     char buf2[64 * MAX_NAME_SIZE];
@@ -265,7 +308,7 @@ int ls_handle(char *params)
     {
         detailed = true;
     }
-    if (ls_dir_inode(&inode_table[current_dir], buf, buf2, detailed) == 0)
+    if (ls_dir_inode(&inode_table[current_dir[user]], buf, buf2, detailed, user) == 0)
     {
         _printf("%s", buf2);
         printf("%s", buf2);
@@ -273,12 +316,11 @@ int ls_handle(char *params)
     return 0;
 }
 
-
 /** @brief 读取文件内容
  * @param params 格式<f>，f为文件名*/
-int cat_handle(char *params)
+int cat_handle(char *params, uint8_t user)
 {
-    if(!check_formatted())
+    if (!check_formatted())
         return 0;
     // char buf[MAX_LINKS_PER_NODE * BLOCK_SIZE];
     char buf[65535];
@@ -288,25 +330,25 @@ int cat_handle(char *params)
     {
         return 0;
     } // 文件名非法
-    if ((ret_id = search_in_dir_inode(&inode_table[current_dir], params, 0)) < 0)
+    if ((ret_id = search_in_dir_inode(&inode_table[current_dir[user]], params, 0, user)) < 0)
     { // 文件不存在
         _printf("File does not exist!\n");
         return 0;
     }
-    if (read_file_inode(&inode_table[ret_id], buf) == 0)
+    if (read_file_inode(&inode_table[ret_id], buf, user) == 0)
     {
         _printf(buf);
         _printf("\n");
     }
     else
-    return 0;
+        return 0;
 }
 
 /** @brief 读取文件内容
  * @param params 格式<f> <l> <data>，f为文件名，l为长度，data为写入数据*/
-int w_handle(char *params)
+int w_handle(char *params, uint8_t user)
 {
-    if(!check_formatted())
+    if (!check_formatted())
         return 0;
     char buf[MAX_LINKS_PER_NODE * BLOCK_SIZE];
     char file_name[MAX_NAME_SIZE];
@@ -318,7 +360,7 @@ int w_handle(char *params)
     {
         return 0;
     } // 文件名非法
-    if ((ret_id = search_in_dir_inode(&inode_table[current_dir], file_name, 0)) < 0)
+    if ((ret_id = search_in_dir_inode(&inode_table[current_dir[user]], file_name, 0, user)) < 0)
     { // 文件不存在
         _printf("File does not exist!\n");
         return 0;
@@ -328,7 +370,7 @@ int w_handle(char *params)
         _printf("\033[1;33mWarning: The string length parameter does not match the actual length!\033[0m\n");
         len = strlen(buf);
     }
-    if (write_file_inode(&inode_table[ret_id], buf) != 0)
+    if (write_file_inode(&inode_table[ret_id], buf, user) != 0)
     {
         _printf("Failed. Disk space may be insufficient\n");
     }
@@ -337,9 +379,9 @@ int w_handle(char *params)
 
 /** @brief 插入
  * @param params 格式<f> <pos> <l> <data>，f为文件名，pos为位置，l为长度，data为写入数据*/
-int i_handle(char *params)
+int i_handle(char *params, uint8_t user)
 {
-    if(!check_formatted())
+    if (!check_formatted())
         return 0;
     char buf[MAX_LINKS_PER_NODE * BLOCK_SIZE];
     char src[MAX_LINKS_PER_NODE * BLOCK_SIZE];
@@ -351,7 +393,7 @@ int i_handle(char *params)
     {
         return 0;
     } // 文件名非法
-    if ((ret_id = search_in_dir_inode(&inode_table[current_dir], file_name, 0)) < 0)
+    if ((ret_id = search_in_dir_inode(&inode_table[current_dir[user]], file_name, 0, user)) < 0)
     { // 文件不存在
         _printf("File does not exist!\n");
         return 0;
@@ -374,7 +416,7 @@ int i_handle(char *params)
         return 0;
     } // 过长
     memset(buf, 0, MAX_LINKS_PER_NODE * BLOCK_SIZE);
-    int ret = read_file_inode(&inode_table[ret_id], buf);
+    int ret = read_file_inode(&inode_table[ret_id], buf, user);
     if (ret < 0)
     {
         return 0;
@@ -385,7 +427,7 @@ int i_handle(char *params)
     memcpy(buf + pos, src, len);                          // 插入数据
     buf[new_size] = '\0';
 
-    if (write_file_inode(&inode_table[ret_id], buf) != 0)
+    if (write_file_inode(&inode_table[ret_id], buf, user) != 0)
     {
         _printf("Failed.\n");
     }
@@ -394,9 +436,9 @@ int i_handle(char *params)
 
 /** @brief 删除
  * @param params 格式<f> <pos> <l>，f为文件名，pos为位置，l为长度*/
-int d_handle(char *params)
+int d_handle(char *params, uint8_t user)
 {
-    if(!check_formatted())
+    if (!check_formatted())
         return 0;
     char buf[MAX_LINKS_PER_NODE * BLOCK_SIZE];
     char src[MAX_LINKS_PER_NODE * BLOCK_SIZE];
@@ -407,7 +449,7 @@ int d_handle(char *params)
     {
         return 0;
     } // 文件名非法
-    if ((ret_id = search_in_dir_inode(&inode_table[current_dir], file_name, 0)) < 0)
+    if ((ret_id = search_in_dir_inode(&inode_table[current_dir[user]], file_name, 0, user)) < 0)
     { // 文件不存在
         _printf("File does not exist!\n");
         return 0;
@@ -421,7 +463,7 @@ int d_handle(char *params)
     if (pos + len > file_size)
         len = file_size - pos;
     memset(buf, 0, MAX_LINKS_PER_NODE * BLOCK_SIZE);
-    int ret = read_file_inode(&inode_table[ret_id], buf);
+    int ret = read_file_inode(&inode_table[ret_id], buf, user);
     if (ret < 0)
     {
         return 0;
@@ -434,7 +476,7 @@ int d_handle(char *params)
     src[file_size - len] = '\0';
     // _printf("buf: %s\n", buf);
 
-    if (write_file_inode(&inode_table[ret_id], src) != 0)
+    if (write_file_inode(&inode_table[ret_id], src, user) != 0)
     {
         _printf("Failed\n");
     }
@@ -442,21 +484,34 @@ int d_handle(char *params)
 }
 
 /** @brief 退出系统 */
-int exit_handle(char *params)
+int exit_handle(char *params, uint8_t owner)
 {
     _printf("Goodbye!\n");
     return -1; //  返回-1使main中循环退出
 }
-int ini_handle(char *params)
+int ini_handle(char *params, uint8_t id)
 {
-    if(!check_formatted())
+    uint8_t newid = atoi(params);
+    uid[id] = newid;
+    if (!check_formatted())
+    {
+        _printf("!format");
         return 0;
+    }
+    current_dir[uid[id]] = 0;
+    strcpy(cur_path_string[uid[id]], "/");
+    char home[MAX_NAME_SIZE];
+    snprintf(home, MAX_NAME_SIZE, "home%d", uid[id]);
+    char mkd[MAX_NAME_SIZE];
+    snprintf(mkd, MAX_NAME_SIZE, "home%d 2", uid[id]);
+    mkdir_handle(mkd, uid[id]);
+    cd_handle(home, uid[id]);
     return 1;
 }
 typedef struct Command
 {
-    char cmd_head[6];            // 命令名称，对每行输入的首段进行匹配
-    int (*handle)(char *params); // 处理该命令的函数
+    char cmd_head[6];                           // 命令名称，对每行输入的首段进行匹配
+    int (*handle)(char *params, uint8_t owner); // 处理该命令的函数(char *params,uint8_t owner)
 } Command;
 
 Command commands_list[] = {
@@ -472,7 +527,7 @@ Command commands_list[] = {
     {"i", i_handle},
     {"d", d_handle},
     {"e", exit_handle},
-    {"B", ini_handle},
+    {"ini",ini_handle},
 };
 
 const int num_commands = sizeof(commands_list) / sizeof(Command);
@@ -482,7 +537,7 @@ void add_client(int id)
 {
 
     memset(response, 0, sizeof(response));
-    // send_cur_path_string(client_socket);
+    // send_cur_path_string[owner](client_socket);
 
     if (load_spb() >= 0)
     { // 加载spb
@@ -490,16 +545,15 @@ void add_client(int id)
         { // 已有文件系统且核对磁盘块数目正确
             formatted = true;
             for (int i = 0; i < MAX_INODE_COUNT; i++)
-            { // 写回inode table到diskfile
-                if (read_inode(&inode_table[i], i) < 0)
+            {                                           // 写回inode table到diskfile
+                if (read_inode(&inode_table[i], i) < 0) // 0是super user
                 {
                     printf("Read Inode failed.\n");
                     formatted = false;
                     break;
                 }
             }
-            current_dir = 0;
-            strcpy(cur_path_string, "/");
+            
             if (formatted)
                 _printf("Already have formatted file system. We've loaded it.\n");
         }
@@ -508,12 +562,10 @@ void add_client(int id)
     {
         _printf("Don't find formatted file system. Please first format.\n");
     }
-
 }
 
 int handle_client(int id, tcp_buffer *write_buf, char *msg, int len)
 {
-
     char raw_command[COMMAND_LENGTH];
     char cmd_head[6];
     char params[COMMAND_LENGTH];
@@ -522,7 +574,6 @@ int handle_client(int id, tcp_buffer *write_buf, char *msg, int len)
     memset(response, 0, sizeof(response));
     memset(params, 0, sizeof(params));
     memcpy(raw_command, msg, sizeof(raw_command));
-
     bool handled = false;
     // 初始化响应字符串
     for (int i = 0; i < num_commands; i++)
@@ -530,18 +581,18 @@ int handle_client(int id, tcp_buffer *write_buf, char *msg, int len)
         sscanf(raw_command, "%s %[^\n]%*c", cmd_head, params);
         if (strcmp(cmd_head, commands_list[i].cmd_head) == 0)
         {
-            ret = commands_list[i].handle(params);
+            ret = commands_list[i].handle(params, id);
             // if (!strlen(response)) _printf("Done.\n");
             handled = true;
-            _printf(cur_path_string);
+            _printf(cur_path_string[uid[id]]);
             _printf("$");
-            send_to_buffer(write_buf,response,strlen(response));
+            send_to_buffer(write_buf, response, strlen(response));
             return 0;
         }
     }
-    char msg0[MAX_PATH_STR_LENGTH+10]="!handled\n";
-    strcat(msg0,cur_path_string);
-    strcat(msg0,"$");
+    char msg0[MAX_PATH_STR_LENGTH + 10] = "!handled\n";
+    strcat(msg0, cur_path_string[uid[id]]);
+    strcat(msg0, "$");
     if (!handled)
     {
         send_to_buffer(write_buf, msg0, strlen(msg0) + 1);
@@ -568,6 +619,7 @@ int main(int argc, char *argv[])
                 argv[0]);
         exit(EXIT_FAILURE);
     }
+    log_file = fopen("fs.log", "w");
     int port_server = atoi(argv[2]);
     int port_client = atoi(argv[1]);
     client = client_init("localhost", port_client);
